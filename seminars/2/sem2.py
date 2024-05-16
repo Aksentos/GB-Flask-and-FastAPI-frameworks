1053ae3dac773aed4d05726d84c8fd71eb9e2c8e
Ses <sesaks@mail.ru>
2024-05-16 20:45:21 +0300

    Add task 3 to sem 2

--- seminars/2/sem2.py
+++ seminars/2/sem2.py
@@ -45,29 +45,51 @@ def upload_image():
     if request.method == "POST":
         file = request.files.get("file")
         file_name = secure_filename(file.filename)
-        if file_name.endswith(('jpg', 'jpeg', 'png')):
+        if file_name.endswith(("jpg", "jpeg", "png")):
             file.save(PurePath.joinpath(Path.cwd(), "static/images", file_name))
             return f"Картинка {file_name} загружена на сервер"
 
     return render_template("upload.html", title="Загрузка картинки на сервер")
 
 
-'''
+"""
 Задание №3
 Создать страницу, на которой будет форма для ввода логина и пароля
 При нажатии на кнопку "Отправить" будет произведена
 проверка соответствия логина и пароля и переход на
 страницу приветствия пользователя или страницу с
 ошибкой.
-'''
-from 
+"""
+from flask import redirect
+
+USERS = {"Alex": "1234", "Admin": "1111"}
 
 
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    context = {"message": None, "url": request.base_url}
+
+    if request.method == "POST":
+        name = request.form.get("name")
+        password = request.form.get("password")
+
+        if name in USERS:
+            if USERS[name] == password:
+                return render_template("hello.html", name=name)
+            context["message"] = f"Неверный пароль"
+            return render_template("404.html", **context), 404
+        context["message"] = f"Пользователь с таким именем не зарегистрирован"
+        return render_template("404.html", **context), 404
+    return render_template("login.html", title="Страница входа")
 
 
-
-
-
+@app.errorhandler(404)
+def page_not_found(e):
+    context = {
+        "title": "Страница не найдена",
+        "url": request.base_url,
+    }
+    return render_template("404.html", **context), 404
 
 
 if __name__ == "__main__":
