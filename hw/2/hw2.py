335f83f86774b8c5f38eb9cb666bcf7c067fa318
Ses <sesaks@mail.ru>
2024-05-17 20:18:00 +0300

    Update hw2

--- hw/2/hw2.py
+++ hw/2/hw2.py
@@ -11,35 +11,49 @@
 пользователя и произведено перенаправление на страницу
 ввода имени и электронной почты.
 """
-from multiprocessing import context
-from flask import Flask, render_template, request, make_response, redirect, url_for
+
+from flask import (
+    Flask,
+    render_template,
+    request,
+    make_response,
+    redirect,
+    url_for,
+)
 
 app = Flask(__name__)
+app.secret_key = "816b422c25127e8a51af86a65e2821b3c23bc6407baa64f37281382b503b204a"
 
 
-@app.route('/')
-@app.route('/index/')
+@app.route("/")
+@app.route("/index/")
 def index():
     name = request.cookies.get("username")
+    print(f"index - {name}")
     if name:
         context = {"name": name}
-        response = make_response(render_template('index.html'), **context)
+        response = make_response(render_template("index.html", **context))
         return response
-        
-    return redirect(url_for('log_in'))
 
-@app.route('/login/', methods=["GET", "POST"])
-def log_in():
+    return redirect(url_for("login"))
+
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
     context = {"title": "Страница входа"}
     if request.method == "POST":
         name = request.form.get("name")
         mail = request.form.get("mail")
-        response = make_response(render_template('index.html'))
+        context["name"] = name
+        response = make_response(render_template("index.html", **context))
         response.set_cookie("username", name)
-        response.set_cookie("username", mail)
-        return redirect(url_for('index'))
-                           
-    return render_template("login.html", **context)
+        response.set_cookie("usermail", mail)
+        return response
+    response = make_response(render_template("login.html"))
+    response.delete_cookie("username")
+    response.delete_cookie("usermail")
+    return response
+
 
 if __name__ == "__main__":
     app.run(debug=True)
