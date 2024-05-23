e8dad64f7a6745cc8870a37e138c4a89093522a4
Ses <sesaks@mail.ru>
2024-05-23 21:14:00 +0300

    Add task 4

--- seminars/4/task_4.py
+++ seminars/4/task_4.py
@@ -1,1 +1,34 @@
+"""
+Задание №4
+� Создать программу, которая будет производить подсчет
+количества слов в каждом файле в указанной директории и
+выводить результаты в консоль.
+� Используйте потоки.
+"""
+
 import threading
+from pathlib import Path
+
+
+def words_count(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        text = f.read()
+        text = [
+            word
+            for word in text.replace(".", "").replace("/n", " ").split()
+            if word.isalpha()
+        ]
+        print(f"В файле {filename.stem} количество слов = {len(text)}")
+
+
+path = Path(Path().cwd() / "texts")
+threads = []
+
+for obj in path.iterdir():
+    if obj.is_file():
+        thread = threading.Thread(target=words_count, args=(obj,))
+        threads.append(thread)
+        thread.start()
+
+for thread in threads:
+    thread.join()
