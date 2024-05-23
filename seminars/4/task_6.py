b92979a096ec0da7c92b41c47c2fb64eecda7d61
Ses <sesaks@mail.ru>
2024-05-23 21:33:28 +0300

    Add task 6

---
+++ seminars/4/task_6.py
@@ -0,0 +1,40 @@
+"""
+Задание №6
+� Создать программу, которая будет производить подсчет
+количества слов в каждом файле в указанной директории и
+выводить результаты в консоль.
+� Используйте асинхронный подход.
+"""
+
+import asyncio
+from pathlib import Path
+
+
+async def words_count(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        text = f.read()
+        text = [
+            word
+            for word in text.replace(".", "")
+            .replace(",", "")
+            .replace("/n", " ")
+            .split()
+            if word.isalpha()
+        ]
+        print(f"В файле {filename.stem} количество слов = {len(text)}")
+
+
+async def main():
+    path = Path(Path().cwd() / "texts")
+    tasks = []
+
+    for obj in path.iterdir():
+        if obj.is_file():
+            task = asyncio.ensure_future(words_count(obj))
+            tasks.append(task)
+    await asyncio.gather(*tasks)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
