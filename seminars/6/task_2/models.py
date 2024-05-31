ec683f3bda2ee5dccd04ced5e43504d313941012
Aksentos <you@example.com>
2024-05-31 13:00:19 +0300

    Update task 1 models in seminar 6

--- seminars/6/task_2/models.py
+++ seminars/6/task_2/models.py
@@ -2,18 +2,14 @@ from datetime import date
 from pydantic import BaseModel, EmailStr, Field
 
 
-class User(BaseModel):
-    id: int
-    name: str = Field(..., title="Name", max_length=20)
-    surname: str = Field(..., title="surname", max_length=20)
-    birthday: date = Field(...,  title="Birthday", description="YYYY-MM-DD")
-    email: EmailStr = Field(..., title="Email", max_length=128)
-    address: str = Field(title="Address", max_length=128)
 
 
 class UserIn(BaseModel):
-    name: str = Field(..., title="Name", max_length=20)
-    surname: str = Field(..., title="surname", max_length=20)
-    birthday: date = Field(...,  title="Birthday", description="YYYY-MM-DD")
+    name: str = Field(..., title="Name", min_length=2)
+    surname: str = Field(..., title="surname", min_length=2)
+    birthday: date = Field(..., title="Birthday", description="YYYY-MM-DD")
     email: EmailStr = Field(..., title="Email", max_length=128)
-    address: str = Field(title="Address", max_length=128)
+    address: str = Field(title="Address", min_length=5, max_length=128)
+
+class User(UserIn):
+    id: int
